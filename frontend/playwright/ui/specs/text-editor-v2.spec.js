2a4fb3f34ffa4f05da83d22b4f9e45af9be3e34a
Aitor Moreno <asakon28@gmail.com>
2025-10-30 14:15:12 +0100

    :sparkles: Add text editor v2 integration tests

--- frontend/playwright/ui/specs/text-editor-v2.spec.js
+++ frontend/playwright/ui/specs/text-editor-v2.spec.js
@@ -1,9 +1,218 @@
 import { test, expect } from "@playwright/test";
 import { WorkspacePage } from "../pages/WorkspacePage";
 
+const timeoutToWait = 50
+
 test.beforeEach(async ({ page }) => {
   await WorkspacePage.init(page);
   await WorkspacePage.mockConfigFlags(page, ["enable-feature-text-editor-v2"]);
+});
+
+test("Create a new text shape", async ({ page }) => {
+  const textToPaste = "Lorem ipsum";
+  const workspace = new WorkspacePage(page);
+  await workspace.setupEmptyFile();
+  await workspace.mockGetFile("workspace/get-file-blank.json");
+  await workspace.goToWorkspace({
+    fileId: "c7ce0794-0992-8105-8004-38f280443849",
+    pageId: "66697432-c33d-8055-8006-2c62cc084cad",
+  });
+  await workspace.page.keyboard.press("T");
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.clickAndMove(190, 150, 300, 200);
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.page.keyboard.type(textToPaste);
+
+  const firstInline = await workspace.page.waitForSelector('[data-itype="inline"]');
+  const textContent = await firstInline.textContent();
+
+  expect(textContent).toBe(textToPaste);
+
+  await workspace.page.keyboard.press("Escape");
+});
+
+test("Create a new text shape from pasting text", async ({ page, context }) => {
+  const textToPaste = "Lorem ipsum";
+  const workspace = new WorkspacePage(page);
+  await workspace.setupEmptyFile();
+  await workspace.mockGetFile("text-editor/get-file-blank.json");
+  await workspace.mockRPC(
+    "update-file?id=*",
+    "text-editor/update-file.json",
+  );
+  await workspace.goToWorkspace({
+    fileId: "c7ce0794-0992-8105-8004-38f280443849",
+    pageId: "66697432-c33d-8055-8006-2c62cc084cad",
+  });
+  await context.grantPermissions(["clipboard-read", "clipboard-write"]);
+  await workspace.clickAt(190, 150);
+  await page.waitForTimeout(timeoutToWait);
+  await page.evaluate((textToPaste) => navigator.clipboard.writeText(textToPaste), textToPaste);
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.page.keyboard.press("Control+V");
+  await page.waitForTimeout(timeoutToWait);
+  const firstInline = await page.waitForSelector('[data-itype="inline"]');
+  const textContent = await firstInline.textContent();
+  expect(textContent).toBe(textToPaste);
+  await workspace.page.keyboard.press("Escape");
+});
+
+test("Create a new text shape from pasting text using context menu", async ({ page, context }) => {
+  const textToPaste = "Lorem ipsum";
+  const workspace = new WorkspacePage(page);
+  await workspace.setupEmptyFile();
+  await workspace.mockGetFile("text-editor/get-file-blank.json");
+  await workspace.mockRPC(
+    "update-file?id=*",
+    "text-editor/update-file.json",
+  );
+  await workspace.goToWorkspace({
+    fileId: "c7ce0794-0992-8105-8004-38f280443849",
+    pageId: "66697432-c33d-8055-8006-2c62cc084cad",
+  });
+  await context.grantPermissions(["clipboard-read", "clipboard-write"]);
+  await workspace.clickAt(190, 150);
+  await page.waitForTimeout(timeoutToWait);
+  await page.evaluate((textToPaste) => navigator.clipboard.writeText(textToPaste), textToPaste);
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.viewport.click({ button: "right" });
+  await page.waitForTimeout(timeoutToWait);
+  await page.getByText("PasteCtrlV").click();
+  const firstInline = await page.waitForSelector('[data-itype="inline"]');
+  const textContent = await firstInline.textContent();
+  expect(textContent).toBe(textToPaste);
+  await workspace.page.keyboard.press("Escape");
+})
+
+test("Update an already created text shape by appending text", async ({ page }) => {
+  const workspace = new WorkspacePage(page);
+  await workspace.setupEmptyFile();
+  await workspace.mockGetFile("text-editor/get-file-lorem-ipsum.json");
+  await workspace.goToWorkspace({
+    fileId: "c7ce0794-0992-8105-8004-38f280443849",
+    pageId: "66697432-c33d-8055-8006-2c62cc084cad",
+  });
+  await workspace.clickLeafLayer("Lorem ipsum");
+  await page.waitForTimeout(timeoutToWait);
+  await page.keyboard.press("Enter");
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.page.keyboard.press("ArrowRight");
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.page.keyboard.type(" dolor sit amet")
+  await page.waitForTimeout(timeoutToWait);
+  const firstInline = await page.waitForSelector('[data-itype="inline"]');
+  const textContent = await firstInline.textContent();
+  expect(textContent).toBe("Lorem ipsum dolor sit amet");
+  await workspace.page.keyboard.press("Escape");
+});
+
+test("Update an already created text shape by prepending text", async ({
+  page,
+}) => {
+  const workspace = new WorkspacePage(page);
+  await workspace.setupEmptyFile();
+  await workspace.mockGetFile("text-editor/get-file-lorem-ipsum.json");
+  await workspace.goToWorkspace({
+    fileId: "c7ce0794-0992-8105-8004-38f280443849",
+    pageId: "66697432-c33d-8055-8006-2c62cc084cad",
+  });
+  await workspace.clickLeafLayer("Lorem ipsum");
+  await page.waitForTimeout(timeoutToWait);
+  await page.keyboard.press("Enter");
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.page.keyboard.press("ArrowLeft");
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.page.keyboard.type("Dolor sit amet ");
+  await page.waitForTimeout(timeoutToWait);
+  const firstInline = await page.waitForSelector('[data-itype="inline"]');
+  const textContent = await firstInline.textContent();
+  expect(textContent).toBe("Dolor sit amet Lorem ipsum");
+  await workspace.page.keyboard.press("Escape");
+});
+
+test("Update an already created text shape by inserting text in between", async ({
+  page,
+}) => {
+  const workspace = new WorkspacePage(page);
+  await workspace.setupEmptyFile();
+  await workspace.mockGetFile("text-editor/get-file-lorem-ipsum.json");
+  await workspace.goToWorkspace({
+    fileId: "c7ce0794-0992-8105-8004-38f280443849",
+    pageId: "66697432-c33d-8055-8006-2c62cc084cad",
+  });
+  await workspace.clickLeafLayer("Lorem ipsum");
+  await page.waitForTimeout(timeoutToWait);
+  await page.keyboard.press("Enter");
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.page.keyboard.press("ArrowLeft");
+  for (let i = 0; i < 5; i++) {
+    await workspace.page.keyboard.press("ArrowRight");
+  }
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.page.keyboard.type(" dolor sit amet");
+  await page.waitForTimeout(timeoutToWait);
+  const firstInline = await page.waitForSelector('[data-itype="inline"]');
+  const textContent = await firstInline.textContent();
+  expect(textContent).toBe("Lorem dolor sit amet ipsum");
+  await workspace.page.keyboard.press("Escape");
+});
+
+test("Update a new text shape appending text by pasting text", async ({ page }) => {
+  const textToPaste = " dolor sit amet";
+  const workspace = new WorkspacePage(page);
+  await workspace.setupEmptyFile();
+  await workspace.mockGetFile("text-editor/get-file-lorem-ipsum.json");
+  await workspace.goToWorkspace({
+    fileId: "c7ce0794-0992-8105-8004-38f280443849",
+    pageId: "66697432-c33d-8055-8006-2c62cc084cad",
+  });
+  await workspace.clickLeafLayer("Lorem ipsum");
+  await page.waitForTimeout(timeoutToWait);
+  await page.keyboard.press("Enter");
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.page.keyboard.press("ArrowRight");
+  await page.waitForTimeout(timeoutToWait);
+  await page.evaluate(
+    (textToPaste) => navigator.clipboard.writeText(textToPaste),
+    textToPaste,
+  );
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.page.keyboard.press("Control+V");
+  await page.waitForTimeout(timeoutToWait);
+  const firstInline = await page.waitForSelector('[data-itype="inline"]');
+  const textContent = await firstInline.textContent();
+  expect(textContent).toBe("Lorem ipsum dolor sit amet");
+  await workspace.page.keyboard.press("Escape");
+});
+
+test("Update a new text shape prepending text by pasting text", async ({
+  page,
+}) => {
+  const textToPaste = "Dolor sit amet ";
+  const workspace = new WorkspacePage(page);
+  await workspace.setupEmptyFile();
+  await workspace.mockGetFile("text-editor/get-file-lorem-ipsum.json");
+  await workspace.goToWorkspace({
+    fileId: "c7ce0794-0992-8105-8004-38f280443849",
+    pageId: "66697432-c33d-8055-8006-2c62cc084cad",
+  });
+  await workspace.clickLeafLayer("Lorem ipsum");
+  await page.waitForTimeout(timeoutToWait);
+  await page.keyboard.press("Enter");
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.page.keyboard.press("ArrowLeft");
+  await page.waitForTimeout(timeoutToWait);
+  await page.evaluate(
+    (textToPaste) => navigator.clipboard.writeText(textToPaste),
+    textToPaste,
+  );
+  await page.waitForTimeout(timeoutToWait);
+  await workspace.page.keyboard.press("Control+V");
+  await page.waitForTimeout(timeoutToWait);
+  const firstInline = await page.waitForSelector('[data-itype="inline"]');
+  const textContent = await firstInline.textContent();
+  expect(textContent).toBe("Dolor sit amet Lorem ipsum");
+  await workspace.page.keyboard.press("Escape");
 });
 
 test.skip("BUG 11552 - Apply styles to the current caret", async ({ page }) => {
@@ -14,7 +223,6 @@ test.skip("BUG 11552 - Apply styles to the current caret", async ({ page }) => {
     "update-file?id=*",
     "text-editor/update-file-11552.json",
   );
-
   await workspace.goToWorkspace({
     fileId: "238a17e0-75ff-8075-8006-934586ea2230",
     pageId: "238a17e0-75ff-8075-8006-934586ea2231",
--- frontend/text-editor/src/editor/controllers/SelectionController.js
+++ frontend/text-editor/src/editor/controllers/SelectionController.js
@@ -275,7 +275,7 @@ export class SelectionController extends EventTarget {
    *
    * @param {HTMLElement} element
    */
-  #applyStylesToCurrentStyle(element) {
+  #applyStylesFromElementToCurrentStyle(element) {
     for (let index = 0; index < element.style.length; index++) {
       const styleName = element.style.item(index);
 
@@ -297,10 +297,10 @@ export class SelectionController extends EventTarget {
   #updateCurrentStyle(textSpan) {
     this.#applyDefaultStylesToCurrentStyle();
     const root = textSpan.parentElement.parentElement;
-    this.#applyStylesToCurrentStyle(root);
+    this.#applyStylesFromElementToCurrentStyle(root);
     const paragraph = textSpan.parentElement;
-    this.#applyStylesToCurrentStyle(paragraph);
-    this.#applyStylesToCurrentStyle(textSpan);
+    this.#applyStylesFromElementToCurrentStyle(paragraph);
+    this.#applyStylesFromElementToCurrentStyle(textSpan);
     return this;
   }
 
@@ -359,7 +359,7 @@ export class SelectionController extends EventTarget {
     // If focus node changed, we need to retrieve all the
     // styles of the current text span and dispatch an event
     // to notify that the styles have changed.
-    if (focusNodeChanges) {
+    if (focusNodeChanges || anchorNodeChanges) {
       this.#notifyStyleChange();
     }
 
@@ -378,25 +378,30 @@ export class SelectionController extends EventTarget {
    * Notifies that the styles have changed.
    */
   #notifyStyleChange() {
-    const textSpan = this.focusTextSpan;
-    if (textSpan) {
-      this.#updateCurrentStyle(textSpan);
-      this.dispatchEvent(
-        new CustomEvent("stylechange", {
-          detail: this.#currentStyle,
-        }),
-      );
-    } else {
-      const firstTextSpan =
-        this.#textEditor.root?.firstElementChild?.firstElementChild;
-      if (firstTextSpan) {
-        this.#updateCurrentStyle(firstTextSpan);
+    if (this.#range.isCollapsed) {
+      // CARET
+      const textSpan = this.focusTextSpan;
+      if (textSpan) {
+        this.#updateCurrentStyle(textSpan);
         this.dispatchEvent(
           new CustomEvent("stylechange", {
             detail: this.#currentStyle,
           }),
         );
+      } else {
+        const firstTextSpan =
+          this.#textEditor.root?.firstElementChild?.firstElementChild;
+        if (firstTextSpan) {
+          this.#updateCurrentStyle(firstTextSpan);
+          this.dispatchEvent(
+            new CustomEvent("stylechange", {
+              detail: this.#currentStyle,
+            }),
+          );
+        }
       }
+    } else {
+      // SELECTION.
     }
   }
 
@@ -426,6 +431,22 @@ export class SelectionController extends EventTarget {
       }
     }
     document.addEventListener("selectionchange", this.#onSelectionChange);
+  }
+
+  /**
+   * Disposes the current resources.
+   */
+  dispose() {
+    document.removeEventListener("selectionchange", this.#onSelectionChange);
+    this.#textEditor = null;
+    this.#ranges.clear();
+    this.#ranges = null;
+    this.#range = null;
+    this.#selection = null;
+    this.#focusNode = null;
+    this.#anchorNode = null;
+    this.#mutations.dispose();
+    this.#mutations = null;
   }
 
   /**
@@ -502,6 +523,8 @@ export class SelectionController extends EventTarget {
    * Marks the start of a mutation.
    *
    * Clears all the mutations kept in CommandMutations.
+   *
+   * @returns {boolean}
    */
   startMutation() {
     this.#mutations.clear();
@@ -512,7 +535,7 @@ export class SelectionController extends EventTarget {
   /**
    * Marks the end of a mutation.
    *
-   * @returns
+   * @returns {CommandMutations}
    */
   endMutation() {
     return this.#mutations;
@@ -520,6 +543,8 @@ export class SelectionController extends EventTarget {
 
   /**
    * Selects all content.
+   *
+   * @returns {SelectionController}
    */
   selectAll() {
     if (this.#textEditor.isEmpty) {
@@ -575,6 +600,8 @@ export class SelectionController extends EventTarget {
 
   /**
    * Moves cursor to end.
+   *
+   * @returns {SelectionController}
    */
   cursorToEnd() {
     const range = document.createRange(); //Create a range (a range is a like the selection but invisible)
@@ -660,22 +687,6 @@ export class SelectionController extends EventTarget {
         );
       }
     }
-  }
-
-  /**
-   * Disposes the current resources.
-   */
-  dispose() {
-    document.removeEventListener("selectionchange", this.#onSelectionChange);
-    this.#textEditor = null;
-    this.#ranges.clear();
-    this.#ranges = null;
-    this.#range = null;
-    this.#selection = null;
-    this.#focusNode = null;
-    this.#anchorNode = null;
-    this.#mutations.dispose();
-    this.#mutations = null;
   }
 
   /**
@@ -1131,11 +1142,15 @@ export class SelectionController extends EventTarget {
    * @param {DocumentFragment} fragment
    */
   insertPaste(fragment) {
+    const hasOnlyOneParagraph = fragment.children.length === 1;
+    const forceTextSpan =
+      fragment.firstElementChild?.dataset?.textSpan === "force";
     if (
-      fragment.children.length === 1 &&
-      fragment.firstElementChild?.dataset?.textSpan === "force"
+      hasOnlyOneParagraph &&
+      forceTextSpan
     ) {
-      const collapseNode = fragment.firstElementChild.firstChild;
+      // first text span
+      const collapseNode = fragment.firstElementChild.firstElementChild;
       if (this.isTextSpanStart) {
         this.focusTextSpan.before(...fragment.firstElementChild.children);
       } else if (this.isTextSpanEnd) {
@@ -1147,7 +1162,9 @@ export class SelectionController extends EventTarget {
           newTextSpan,
         );
       }
-      return this.collapse(collapseNode, collapseNode.nodeValue?.length || 0);
+      // collapseNode could be a <br>, that's why we need to
+      // make `nodeValue` as optional.
+      return this.collapse(collapseNode, collapseNode?.nodeValue?.length || 0);
     }
     const collapseNode = this.#getFragmentParagraphTextNode(fragment);
     if (this.isParagraphStart) {
